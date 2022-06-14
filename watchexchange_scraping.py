fd23701fd0a686831835dc95943cec36273637dd
Angel Salazar <aangelsalazarr@gmail.com>
2022-06-14 12:00:02 -0500

    tried to get it to work

--- watchexchange_scraping.py
+++ watchexchange_scraping.py
@@ -4,8 +4,17 @@ import praw
 # information from creating an app
 my_client_id = 'oNiqOTx0INdAvxJ-kEs2Qg'
 my_client_secret = 'b8bgr02-xALNovaZqSgGFAP2eoZTPQ'
-my_user_agent = 'webscraping'
+my_user_agent = 'energy watch webscraping'
 
 # purpose is to authenticate ourselves
-reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret, user_agent=my_user_agent)
+reddit = praw.Reddit(client_id=my_client_id,
+                     client_secret=my_client_secret,
+                     user_agent=my_user_agent)
+
+# we want to get hottest posts from the watch exchange subreddit
+hot_posts_watch_ex = reddit.subreddit('energy').hot(limit=25)
+
+# purpose is to print the top n posts from watch exchange
+for post in hot_posts_watch_ex:
+    print(post.title)
 
